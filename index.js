ed821d2ab798565afc62eeb75165b18dc4fc340f
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 10:39:31 +0100

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function main () {
-    console.log("Starting...");
+function imprimeHola (){
+    console.log ("Hola")
 }
-window.addEventListener("load",main);
+function main () {
+    console.log ("Starting...");
+    const title = document.querySelector ("h1")
+    title.addEventListener("pointerenter", imprimeHola)
+}
+window.addEventListener ("load",main)
 //cuando termine de cargar el documento se va a ejecutar main.
