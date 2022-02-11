096d185e444e9e2ccc35f11a431846019a2472ad
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 12:46:45 +0100

    Added changes 2.0

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 function imprimeHola (){
     console.log ("Hola")
 }
+
+
 function main () {
     console.log ("Starting...");
+   
     const title = document.querySelector ("h1")
     title.addEventListener("pointerenter", imprimeHola)
+    
+    const elements = Array.from(document.body.querySelectorAll("*")); //con esta forma no es necesario hacer crear los const innecesarios.
+    
+   // const elem = document.body.querySelectorAll("*") 
+   // for (let item of elem) {item.addEventListener("pointerenter",()=>{console.log("POP!")})
+
+    const counters = elements.map(
+        (item)=>{
+            return{
+                elements: item,
+                counter: 0,
+            }
+        }
+    );
+    console.log(counters);
 }
-window.addEventListener ("load",main)
-//cuando termine de cargar el documento se va a ejecutar main.
+  
+window.addEventListener ("load",main);
